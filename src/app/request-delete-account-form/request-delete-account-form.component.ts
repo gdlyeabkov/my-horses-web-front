5462d1b5f83d64178343261e40179fb1243a3a68
softtrack <nickfish2020@gmail.com>
2025-05-26 19:51:16 +0300

    bind template with data

--- src/app/request-delete-account-form/request-delete-account-form.component.ts
+++ src/app/request-delete-account-form/request-delete-account-form.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-request-delete-account-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './request-delete-account-form.component.html',
   styleUrl: './request-delete-account-form.component.scss'
 })
 export class RequestDeleteAccountFormComponent {
-
+  email: string = ''
+  reason: string = ''
+  isConfirmed: boolean = false
 }
