2834a5fe99ba311f94f78a48ad9d8390b49ff68d
softtrack <nickfish2020@gmail.com>
2025-05-26 20:55:09 +0300

    add call api

--- src/app/request-delete-account-form/request-delete-account-form.component.ts
+++ src/app/request-delete-account-form/request-delete-account-form.component.ts
@@ -1,18 +1,46 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import {ReactiveFormsModule, Validators, FormBuilder} from '@angular/forms';
+import api from '../../utils/api';
 
 @Component({
   selector: 'app-request-delete-account-form',
-  imports: [FormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './request-delete-account-form.component.html',
   styleUrl: './request-delete-account-form.component.scss',
 })
 export class RequestDeleteAccountFormComponent {
-  email: string = ''
-  reason: string = ''
-  isConfirmed: boolean = false
   status: Status = null
+  private formBuilder = inject(FormBuilder)
+  form = this.formBuilder.group({
+    email: [
+      '',
+      Validators.required
+    ],
+    reason: [
+      '',
+      Validators.required
+    ],
+    isConfirmed: [
+      false,
+      Validators.required
+    ],
+  })
+
+  send = async () => {
+    if (this.status === 'loading') return
+    this.status = 'loading'
+    try {
+      const data = new FormData()
+      data.append('email', this.form.value.email ?? '')
+      data.append('reason', this.form.value.reason ?? '')
+      await api.post('/request-delete-account', data)
+      this.status = 'success'
+      this.form.reset()
+    } catch (e) {
+      this.status = 'error'
+    }
+  }
 }
 
 export type Status = 'success' | 'error' | 'loading' | null
